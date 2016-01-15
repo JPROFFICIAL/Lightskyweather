c0982225587802f64fe835c8911e18f6d9a25f7e
Jordan PR Olson <Jjpprroo15@gmail.com>
2016-01-15 14:04:22 -0600

    Initial Commit

---
+++ LightSkyWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightSkyWeather
+//
+//  Created by Jordan Olson on 1/15/16.
+//  Copyright © 2016 JordanPR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
